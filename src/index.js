073a5e154959358dde1d5964898b9b066896c945
Erfan-brmg <your_email_address@example.com>
2021-10-15 13:30:35 +0800

    home page static

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 
 import App from "./app/layout/App"
 import "./app/layout/styles.css"
+import "semantic-ui-css/semantic.min.css"
 import reportWebVitals from "./reportWebVitals"
 
 const rootEl = document.getElementById("root")
